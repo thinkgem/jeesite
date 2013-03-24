88668e773ef1058f32db0aeacd816d0d6748245e
thinkgem <thinkgem@163.com>
2013-03-24 17:36:32 +0800

    删除mztreeview演示的一些无用js文件

--- src/main/webapp/static/mztreeview/demo/treedata/multimedia.js
+++
@@ -1,8 +0,0 @@
-var data={};
-data['7_701'] = 'text:\u7f51\u9875\u8bbe\u8ba1\uff08Dreamweaver\u7b49\uff09 ; data:roomid=701';
-data['7_702'] = 'text:\u56fe\u8c61\u5de5\u5177\u4f7f\u7528 ; data:roomid=702';
-data['7_703'] = 'text:Flash \u4f7f\u7528 ; data:roomid=703';
-data['7_707'] = 'text:Flash \u5f00\u53d1 ; data:roomid=707';
-data['7_704'] = 'text:\u591a\u5a92\u4f53\u8bbe\u8ba1\uff083DMaX,Maya\u7b49\uff09 ; data:roomid=704';
-data['7_705'] = 'text:\u4ea4\u4e92\u5f0f\u8bbe\u8ba1 ; data:roomid=705';
-data['7_706'] = 'text:AutoCAD ; data:roomid=706';
