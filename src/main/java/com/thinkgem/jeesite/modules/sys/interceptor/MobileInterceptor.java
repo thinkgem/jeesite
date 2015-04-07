1ae876a716ec89fb180bf455da274da21bca2f18
thinkgem <thinkgem@163.com>
2015-04-07 08:51:14 +0800

    增加MobileInterceptor拦截器，显示手机界面，目前只做了手机端登录。

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/interceptor/MobileInterceptor.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.sys.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.thinkgem.jeesite.common.service.BaseService;
+import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.thinkgem.jeesite.common.utils.UserAgentUtils;
+
+/**
+ * 手机端视图拦截器
+ * @author ThinkGem
+ * @version 2014-9-1
+ */
+public class MobileInterceptor extends BaseService implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, 
+			Object handler) throws Exception {
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
+			ModelAndView modelAndView) throws Exception {
+		if (modelAndView != null){
+			// 如果是手机或平板访问的话，则跳转到手机视图页面。
+			if(UserAgentUtils.isMobileOrTablet(request) && !StringUtils.startsWithIgnoreCase(modelAndView.getViewName(), "redirect:")){
+				modelAndView.setViewName("mobile/" + modelAndView.getViewName());
+			}
+		}
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, 
+			Object handler, Exception ex) throws Exception {
+		
+	}
+
+}
