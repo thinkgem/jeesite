097ac054fd308e568a01a83d0bd57697bacbd751
thinkgem <thinkgem@163.com>
2015-05-02 11:29:53 +0800

    MyBatisDao支持自定义bean名称

--- src/main/java/com/thinkgem/jeesite/common/persistence/annotation/MyBatisDao.java
+++ src/main/java/com/thinkgem/jeesite/common/persistence/annotation/MyBatisDao.java
@@ -3,10 +3,13 @@
  */
 package com.thinkgem.jeesite.common.persistence.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
 
 /**
  * 标识MyBatis的DAO,方便{@link org.mybatis.spring.mapper.MapperScannerConfigurer}的扫描。 
@@ -15,6 +18,8 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Documented
+@Component
 public @interface MyBatisDao {
 	
 	/**
