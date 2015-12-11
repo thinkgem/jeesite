7e31672b18d2d568cb12a98f618aad56eb0ee1af
kmlxk <kmlxk@163.com>
2015-12-11 14:03:03 +0800

    * 定义接口参数名称、数据库访问

--- src/main/java/com/thinkgem/jeesite/ws/test/HelloWorldWebServiceImpl.java
+++ src/main/java/com/thinkgem/jeesite/ws/test/HelloWorldWebServiceImpl.java
@@ -1,16 +1,45 @@
 package com.thinkgem.jeesite.ws.test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.jws.WebParam;
 import javax.jws.WebService;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.thinkgem.jeesite.common.utils.SpringContextHolder;
+import com.thinkgem.jeesite.modules.sys.dao.UserDao;
+import com.thinkgem.jeesite.modules.sys.entity.User;
+
+/***
+ * 服务发布后可通过 http://localhost:8080/jeesite/webservice/HelloWorld?wsdl访问
+ * 
+ * */
 
 @WebService()
 public class HelloWorldWebServiceImpl implements HelloWorldWebService {
-	
-	public HelloWorldWebServiceImpl() {
-		System.out.println("HelloWorldWebServiceImpl::constructor()");
-	}
 
 	public String sayHi(String text) {
 		System.out.println("sayHi called");
 		return "Hello " + text;
 	}
+
+	@Override
+	public List<WsResponseUser> userList() {
+		UserDao userDao = SpringContextHolder.getBean(UserDao.class);
+		User query = new User();
+		query.getPage().setPageNo(1);
+		query.getPage().setPageSize(100);
+		List<User> users = userDao.findList(query);
+		List<WsResponseUser> ret = Lists.newArrayList();
+		for (User user : users) {
+			WsResponseUser resp = new WsResponseUser(user.getName(),
+					user.getNo(), user.getLoginName());
+			ret.add(resp);
+		}
+		return ret;
+	}
+
 }
