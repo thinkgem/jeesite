f273b9e146656dfc7b3573ed1e2c11e3ee63f6fc
thinkgem <thinkgem@163.com>
2018-01-01 22:20:21 +0800

    init commit

--- common/src/main/java/com/jeesite/common/network/IpUtils.java
+++ common/src/main/java/com/jeesite/common/network/IpUtils.java
@@ -16,12 +16,9 @@ public class IpUtils {
 		if (request == null) {
 			return "unknown";
 		}
-		String ip = request.getHeader("x-forwarded-for");
+		String ip = request.getHeader("X-Forwarded-For");
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("X-Forwarded-For");
 		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
@@ -29,7 +26,6 @@ public class IpUtils {
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("X-Real-IP");
 		}
-
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getRemoteAddr();
 		}
