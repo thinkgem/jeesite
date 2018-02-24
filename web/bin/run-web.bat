72d5fed50cb27b05486cb4b202ef3f71aa33b8fc
thinkgem <thinkgem@163.com>
2018-02-24 22:38:44 +0800

    优化bat脚本，解压war包后可直接执行startup.bat运行项目。

---
+++ web/bin/run-web.bat
@@ -0,0 +1,26 @@
+@echo off
+rem /**
+rem  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+rem  *
+rem  * Author: ThinkGem@163.com
+rem  */
+echo.
+echo [信息] 打包Web工程，并运行Web工程。
+echo.
+rem pause
+rem echo.
+
+%~d0
+cd %~dp0
+
+cd ..
+
+call mvn clean package spring-boot:repackage -Dmaven.test.skip=true -U
+
+cd target/
+call unzip -n *.war -d web
+
+cd web
+call startup.bat
+
+pause
--- web/src/main/webapp/startup.bat
+++ web/src/main/webapp/startup.bat
@@ -3,9 +3,10 @@ rem /**
 rem  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
 rem  *
 rem  * Author: ThinkGem@163.com
+rem  * 
 rem  */
 echo.
-echo [信息] 打包Web工程，并运行Web工程。
+echo [信息] 运行Web工程。
 echo.
 rem pause
 rem echo.
@@ -13,18 +14,10 @@ rem echo.
 %~d0
 cd %~dp0
 
-cd ../
-call mvn clean package spring-boot:repackage -Dmaven.test.skip=true -U
-
 title %cd%
 
-rem set JAVA_OPTS= -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m
 set JAVA_OPTS= -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-cd target/
-call unzip -n jeesite-web.war -d jeesite-web
-
-cd jeesite-web
 call java org.springframework.boot.loader.WarLauncher
 
 pause
