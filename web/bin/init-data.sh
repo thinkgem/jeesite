86308731005f6ab4e0d6ddc7811b467b04f61600
thinkgem <thinkgem@163.com>
2018-04-08 22:37:43 +0800

    update bin sh

--- web/bin/init-data.sh
+++ web/bin/init-data.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
 echo "[信息] 初始化数据库，请谨慎操作。\n"
-read -s -n1 -p "按任意键继续 ... "
+read -s -n1 -p "请按任意键继续 ... "
 echo "\n[信息] 此操作会清空您的现有数据表，并恢复初始状态。\n"
-read -s -n1 -p "按任意键继续 ... "
-echo "\n[信息] 确认继续吗？否则请关闭窗口。\n"
-read -s -n1 -p "按任意键继续 ... "
+read -s -n1 -p "请按任意键继续 ... "
+echo "\n[信息] 确认继续吗？否则请关闭窗口。（5）\n"
+read -s -n1 -p "请按任意键继续 ... "
+echo "\n[信息] 您真的确认继续吗？否则请关闭窗口。（4）\n"
+read -s -n1 -p "请按任意键继续 ... "
+echo "\n[信息] 您真的确认继续吗？否则请关闭窗口。（3）\n"
+read -s -n1 -p "请按任意键继续 ... "
+echo "\n[信息] 您真的确认继续吗？否则请关闭窗口。（2）\n"
+read -s -n1 -p "请按任意键继续 ... "
+echo "\n[信息] 您真的确认继续吗？否则请关闭窗口。（1）\n"
+read -s -n1 -p "请按任意键继续 ... "
 
 cd ..
 mvn test -Dtest=com.jeesite.test.InitCoreData,com.jeesite.test.InitGenData
--- web/bin/run-tomcat.sh
+++ web/bin/run-tomcat.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# /**
-#  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
-#  *
-#  * Author: ThinkGem@163.com
-#  */
+cd ..
 
-cd ../
-
-# MAVEN_OPTS=$MAVEN_OPTS -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m
 MAVEN_OPTS=$MAVEN_OPTS -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
 exec mvn clean spring-boot:run -U
