1e19211f93aab6a216fc9099028bf2ecd39dac16
thinkgem <thinkgem@163.com>
2018-05-27 22:23:55 +0800

    update script bin

--- web/bin/run-tomcat.sh
+++ web/bin/run-tomcat.sh
@@ -11,4 +11,4 @@ echo ""
 
 cd ..
 MAVEN_OPTS="$MAVEN_OPTS -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m"
-mvn clean spring-boot:run -U
+mvn clean spring-boot:run -Dmaven.test.skip=true -U
