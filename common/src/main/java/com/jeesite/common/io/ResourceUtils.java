26ee250d4a2d6b588403deeb554521c32895a6c9
thinkgem <thinkgem@163.com>
2018-07-02 23:23:11 +0800

    ResourceUtils工具类优化

--- common/src/main/java/com/jeesite/common/io/ResourceUtils.java
+++ common/src/main/java/com/jeesite/common/io/ResourceUtils.java
@@ -10,6 +10,7 @@ import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import com.jeesite.common.lang.ExceptionUtils;
 
@@ -20,7 +21,12 @@ import com.jeesite.common.lang.ExceptionUtils;
  */
 public class ResourceUtils extends org.springframework.util.ResourceUtils {
 	
-	private static ResourceLoader resourceLoader = new DefaultResourceLoader();
+	private static ResourceLoader resourceLoader;
+	private static ResourcePatternResolver resourceResolver;
+	static{
+		resourceLoader = new DefaultResourceLoader();
+		resourceResolver = new PathMatchingResourcePatternResolver(resourceLoader);
+	}
 	
 	/**
 	 * 获取资源加载器（可读取jar内的文件）
@@ -79,11 +85,7 @@ public class ResourceUtils extends org.springframework.util.ResourceUtils {
 	 */
 	public static Resource[] getResources(String locationPattern){
 		try {
-			Resource[] resources = new PathMatchingResourcePatternResolver()
-					.getResources(locationPattern);
-//			System.out.println("===========\n===========");
-//			System.out.println(locationPattern + "   :   " + resources.length);
-//			System.out.println("===========\n===========");
+			Resource[] resources = resourceResolver.getResources(locationPattern);
 			return resources;
 		} catch (IOException e) {
 			throw ExceptionUtils.unchecked(e);
