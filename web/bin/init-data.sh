378bec08f2c24ecbf8748d0bcb0287473de38884
thinkgem <thinkgem@163.com>
2018-07-11 23:10:08 +0800

    为了防止初始化数据表单元测试误运行，增加 -Djeesite.initdata=true 参数作为校验。

--- web/bin/init-data.sh
+++ web/bin/init-data.sh
@@ -29,4 +29,4 @@ read -s -n1 -p "请按任意键继续 ... "
 echo ""
 
 cd ..
-mvn test -Dmaven.test.skip=false -Dtest=com.jeesite.test.InitCoreData -U
+mvn test -Dmaven.test.skip=false -Dtest=com.jeesite.test.InitCoreData -Djeesite.initdata=true -U
