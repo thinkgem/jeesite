e2c546cc6259f921a4bf482f73347a0d42f61bd6
thinkgem <thinkgem@163.com>
2019-02-04 17:04:13 +0800

    js.text()的语言配置文件移动到core项目下

---
+++ modules/core/src/main/resources/static/common/i18n/jeesite_en.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * 
+ * 国际化文件
+ * @author ThinkGem
+ * @version 2017-12-24
+ */
+(function($) {
+window.js = window.js || {};
+js.i18n = $.extend({}, js.i18n, {
+	'tabpanel.newTabPage':'New Page',
+	'loading.message':'Loading ...',
+	'loading.submitMessage':'Submission ...',
+	'showMessage.error':'failure,error',
+	'showMessage.success':'success,completion',
+	'showMessage.warning':'no'
+});
+})(jQuery);
---
+++ modules/core/src/main/resources/static/common/i18n/jeesite_zh_CN.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * 
+ * 国际化文件
+ * @author ThinkGem
+ * @version 2017-12-24
+ */
+(function($) {
+window.js = window.js || {};
+js.i18n = $.extend({}, js.i18n, {
+	'tabpanel.newTabPage':'新页签',
+	'loading.message':'正在加载，请稍后...',
+	'loading.submitMessage':'正在提交，请稍后...',
+	'showMessage.error':'失败,错误,未完成',
+	'showMessage.success':'成功,完成',
+	'showMessage.warning':'不能,不允许,必须,已存在,不需要,不正确'
+});
+})(jQuery);
