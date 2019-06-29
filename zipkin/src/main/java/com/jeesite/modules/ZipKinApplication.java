6cd9286e16694f1765f7a84db7638a880502c97e
长春叭哥 <admin@chinadays.cn>
2019-06-29 22:23:17 +0800

    新增Spring Cloud ZipKin  服务链路追踪

---
+++ zipkin/src/main/java/com/jeesite/modules/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package com.jeesite.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+
+/**
+ * ZipKinApplication
+ * @author 长春叭哥
+ * @version 2019-06-28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
