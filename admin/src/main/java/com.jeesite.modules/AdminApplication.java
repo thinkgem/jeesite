24a22cc90c3488c27e303ceeefec30d97e089568
长春叭哥 <admin@chinadays.cn>
2019-06-29 22:21:32 +0800

    新增Spring Cloud Admin 服务状态监控

---
+++ admin/src/main/java/com.jeesite.modules/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.jeesite.modules;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+/**
+ * AdminApplication
+ * @author 长春叭哥
+ * @version 2019-06-28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
