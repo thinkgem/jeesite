198a28fa5651f974c0041996bd91b7b6aaf0bbb5
thinkgem <thinkgem@163.com>
2019-08-18 15:47:26 +0800

    update 4.1.6

--- admin/src/main/java/com/jeesite/modules/AdminApplication.java
+++ admin/src/main/java/com/jeesite/modules/AdminApplication.java
@@ -1,21 +1,22 @@
 package com.jeesite.modules;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin.server.internal.EnableZipkinServer;
-
 
 /**
- * ZipKinApplication
+ * AdminApplication
  * @author 长春叭哥
  * @version 2019-06-28
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZipkinServer
-public class ZipKinApplication {
+@EnableAdminServer
+public class AdminApplication {
+	
     public static void main(String[] args) {
-        SpringApplication.run(ZipKinApplication.class, args);
+        SpringApplication.run(AdminApplication.class, args);
     }
+    
 }
--- zipkin/src/main/java/com/jeesite/modules/ZipkinApplication.java
+++ zipkin/src/main/java/com/jeesite/modules/ZipkinApplication.java
@@ -1,21 +1,22 @@
 package com.jeesite.modules;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+import zipkin.server.internal.EnableZipkinServer;
 
 /**
- * AdminApplication
+ * ZipkinApplication
  * @author 长春叭哥
  * @version 2019-06-28
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAdminServer
-public class AdminApplication {
+@EnableZipkinServer
+public class ZipkinApplication {
+	
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+        SpringApplication.run(ZipkinApplication.class, args);
     }
+    
 }
