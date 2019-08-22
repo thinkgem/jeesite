e7cfdf0d61e40681521dfb6e865f34ba48c4ef5e
thinkgem <thinkgem@163.com>
2019-08-22 12:52:51 +0800

    优化字符串替换方法replaceAll替换为replace

--- common/src/main/java/com/jeesite/common/idgen/IdGenerate.java
+++ common/src/main/java/com/jeesite/common/idgen/IdGenerate.java
@@ -24,7 +24,7 @@ public class IdGenerate {
 	 * 生成UUID, 中间无-分割.
 	 */
 	public static String uuid() {
-		return UUID.randomUUID().toString().replaceAll("-", "");
+		return StringUtils.replace(UUID.randomUUID().toString(),"-", "");
 	}
 	
 	/**
