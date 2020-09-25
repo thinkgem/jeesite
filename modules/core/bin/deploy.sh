9e62a3b2591d5b8fc4db13a625d0ecf6123b3fed
thinkgem <thinkgem@163.com>
2020-09-25 14:30:14 +0800

    增加 shell 打包和部署脚本

---
+++ modules/core/bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# /**
+#  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+#  *
+#  * Author: ThinkGem@163.com
+#  * 
+#  */
+echo ""
+echo "[信息] 部署工程版本到Nexus服务器。"
+echo ""
+
+cd ..
+mvn clean deploy -Dmaven.test.skip=true -Pdeploy
+
+cd bin
