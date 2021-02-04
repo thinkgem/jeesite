55b473ea7ebcc41a5d560310324177a6d5be5986
thinkgem <thinkgem@163.com>
2021-02-04 19:14:29 +0800

    upgrade nacos 1.4.1

--- nacos/src/main/java/com/alibaba/nacos/console/security/nacos/NacosAuthConfig.java
+++ nacos/src/main/java/com/alibaba/nacos/console/security/nacos/NacosAuthConfig.java
@@ -16,10 +16,10 @@
 
 package com.alibaba.nacos.console.security.nacos;
 
+import com.alibaba.nacos.auth.common.AuthConfigs;
+import com.alibaba.nacos.auth.common.AuthSystemTypes;
 import com.alibaba.nacos.console.filter.JwtAuthenticationTokenFilter;
 import com.alibaba.nacos.console.security.nacos.users.NacosUserDetailsServiceImpl;
-import com.alibaba.nacos.core.auth.AuthConfigs;
-import com.alibaba.nacos.core.auth.AuthSystemTypes;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -56,7 +56,9 @@ public class NacosAuthConfig extends WebSecurityConfigurerAdapter {
     public static final String TOKEN_PREFIX = "Bearer ";
     
     public static final String CONSOLE_RESOURCE_NAME_PREFIX = "console/";
-    
+
+    public static final String UPDATE_PASSWORD_ENTRY_POINT = CONSOLE_RESOURCE_NAME_PREFIX + "user/password";
+
     @Autowired
     private Environment env;
     
