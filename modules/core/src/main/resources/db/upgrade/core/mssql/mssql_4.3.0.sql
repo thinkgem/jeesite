feef99a02a93a1d36b84762cf2e9df0702cea5b1
thinkgem <thinkgem@163.com>
2021-07-12 17:11:55 +0800

    v4.3.0

---
+++ modules/core/src/main/resources/db/upgrade/core/mssql/mssql_4.3.0.sql
@@ -0,0 +1,4 @@
+
+-- 字典表新增字典图标
+ALTER TABLE ${_prefix}sys_dict_data 
+ADD dict_icon nvarchar(100);
---
+++ modules/core/src/main/resources/db/upgrade/core/oracle/oracle_4.3.0.sql
@@ -0,0 +1,5 @@
+
+-- 字典表新增字典图标
+ALTER TABLE ${_prefix}sys_dict_data 
+ADD (dict_icon nvarchar2(100));
+COMMENT ON COLUMN ${_prefix}sys_dict_data.dict_icon IS '字典图标';
