aebdb2b5677fdee4429b1d08b365c28f4899fbd0
thinkgem <thinkgem@163.com>
2021-11-22 20:22:03 +0800

    Tab页签增加关闭全部功能；Tab增加图标；字体均调整为14号；侧边栏选项稍微高一点

--- modules/core/src/main/resources/static/wdScrollTab/js/TabPanel_i18n.js
+++ modules/core/src/main/resources/static/wdScrollTab/js/TabPanel_i18n.js
@@ -3,21 +3,23 @@
 	window.TabPanel = window.TabPanel || {};
 	if (window.lang == 'en'){
 		TabPanel.i18n = $.extend({}, TabPanel.i18n, {
-			closeTab: 'Close',
-			refreshTab: 'Refresh',
-			closeOther: 'Close Other',
-			closeLeft: 'Close Left',
-			closeRight: 'Close Right',
-			detachTab: 'Detach Tab'
+			closeTab: '<i class="fa fa-close"></i> Close',
+			refreshTab: '<i class="fa fa-refresh"></i> Refresh',
+			closeOther: '<i class="fa fa-expand"></i> Close Other',
+			closeLeft: '<i class="fa fa-angle-double-left"></i> Close Left',
+			closeRight: '<i class="fa fa-angle-double-right"></i> Close Right',
+			closeAll: '<i class="fa fa-minus"></i> Close All',
+			detachTab: '<i class="fa fa-share"></i> Detach Tab'
 		});
 	} else if (window.lang == 'ja_JP'){
 		TabPanel.i18n = $.extend({}, TabPanel.i18n, {
-			closeTab: '閉じる',
-			refreshTab: '更新',
-			closeOther: '他を閉じる',
-			closeLeft: '左側を閉じる',
-			closeRight: '右側を閉じる',
-			detachTab: '新しい窓が'
+			closeTab: '<i class="fa fa-close"></i> 閉じる',
+			refreshTab: '<i class="fa fa-refresh"></i> 更新',
+			closeOther: '<i class="fa fa-expand"></i> 他を閉じる',
+			closeLeft: '<i class="fa fa-angle-double-left"></i> 左側を閉じる',
+			closeRight: '<i class="fa fa-angle-double-right"></i> 右側を閉じる',
+			closeAll: '<i class="fa fa-minus"></i> 閉じる全体',
+			detachTab: '<i class="fa fa-share"></i> 新しい窓が'
 		});
 	}
 	
