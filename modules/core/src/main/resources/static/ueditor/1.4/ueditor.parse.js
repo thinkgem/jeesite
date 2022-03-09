6e26d9677b8f6fd6c1f0a07390f07f8241fdbd2f
thinkgem <thinkgem@163.com>
2022-03-09 17:06:41 +0800

    remove old jquery

--- modules/core/src/main/resources/static/ueditor/1.4/ueditor.parse.js
+++ modules/core/src/main/resources/static/ueditor/1.4/ueditor.parse.js
@@ -631,33 +631,34 @@ UE.parse.register('charts',function( utils ){
     //加载资源
     function loadResources () {
 
-        loadJQuery();
+//        loadJQuery();
+        loadHighcharts();
 
     }
 
-    function loadJQuery () {
-
-        //不存在jquery， 则加载jquery
-        if ( !window.jQuery ) {
-
-            utils.loadFile(document,{
-                src : resourceRoot + "/third-party/jquery-1.10.2.min.js",
-                tag : "script",
-                type : "text/javascript",
-                defer : "defer"
-            },function(){
-
-                loadHighcharts();
-
-            });
-
-        } else {
-
-            loadHighcharts();
-
-        }
-
-    }
+//    function loadJQuery () {
+//
+//        //不存在jquery， 则加载jquery
+//        if ( !window.jQuery ) {
+//
+//            utils.loadFile(document,{
+//                src : resourceRoot + "/third-party/jquery.min.js",
+//                tag : "script",
+//                type : "text/javascript",
+//                defer : "defer"
+//            },function(){
+//
+//                loadHighcharts();
+//
+//            });
+//
+//        } else {
+//
+//            loadHighcharts();
+//
+//        }
+//
+//    }
 
     function loadHighcharts () {
 
