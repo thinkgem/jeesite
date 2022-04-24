2160e034b26348042bf89509fff1d6f3309c8b8a
thinkgem <thinkgem@163.com>
2022-04-24 12:06:23 +0800

    docker脚本优化

--- nacos/bin/docker-build.bat
+++ nacos/bin/docker-build.bat
@@ -1,4 +1,4 @@
-@echo off
+s@echo off
 rem /**
 rem  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
 rem  * No deletion without permission, or be held responsible to law.
@@ -6,14 +6,14 @@ rem  *
 rem  * Author: ThinkGem@163.com
 rem  */
 echo.
-echo [信息] 打包Web工程，导入到Docker服务。
+echo [信息] 打包Web工程，编译Docker镜像
 echo.
 
 %~d0
 cd %~dp0
 
 cd ..
-call mvn clean package docker:stop docker:remove docker:build docker:run -Dmaven.test.skip=true -U
+call mvn clean package docker:remove docker:build -Dmaven.test.skip=true -U
 
 cd bin
 pause
