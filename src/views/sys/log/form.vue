07e528634ad20f335e805fa015132930a1cb8ecd
thinkgem <thinkgem@163.com>
2022-04-27 21:18:23 +0800

    增加操作日志页面

---
+++ src/views/sys/log/form.vue
@@ -0,0 +1,230 @@
+<!--
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ * @author ThinkGem
+-->
+<template>
+  <BasicDrawer
+    v-bind="$attrs"
+    :showFooter="true"
+    :showOkBtn="false"
+    @register="registerDrawer"
+    width="80%"
+  >
+    <template #title>
+      <Icon :icon="getTitle.icon" class="pr-1 m-1" />
+      <span> {{ getTitle.value }} </span>
+    </template>
+    <BasicForm @register="registerForm" />
+  </BasicDrawer>
+</template>
+<script lang="ts">
+  export default defineComponent({
+    name: 'ViewsSysLogForm',
+  });
+</script>
+<script lang="ts" setup>
+  import { defineComponent, ref, computed, h } from 'vue';
+  import { useI18n } from '/@/hooks/web/useI18n';
+  // import { useMessage } from '/@/hooks/web/useMessage';
+  import { router } from '/@/router';
+  import { Icon } from '/@/components/Icon';
+  import { BasicForm, FormSchema, useForm } from '/@/components/Form';
+  import { BasicDrawer, useDrawerInner } from '/@/components/Drawer';
+  import { Log, logForm } from '/@/api/sys/log';
+  import { isEmpty } from '/@/utils/is';
+
+  const { t } = useI18n('sys.log');
+  // const { showMessage } = useMessage();
+  const record = ref<Log>({} as Log);
+  const getTitle = computed(() => ({
+    icon: router.currentRoute.value.meta.icon || 'ant-design:book-outlined',
+    value: record.value.isNewRecord ? t('新增日志') : t('查看日志'),
+  }));
+
+  const inputFormSchemas: FormSchema[] = [
+    {
+      label: t('日志标题'),
+      field: 'logTitle',
+      component: 'Input',
+      componentProps: {
+        maxlength: 500,
+      },
+      required: true,
+    },
+    {
+      label: t('日志类型'),
+      field: 'logType',
+      component: 'Select',
+      componentProps: {
+        dictType: 'sys_log_type',
+        allowClear: true,
+      },
+      required: true,
+    },
+    {
+      label: t('请求地址'),
+      field: 'requestUriFormat',
+      component: 'Input',
+      componentProps: {
+        maxlength: 500,
+      },
+      required: true,
+      colProps: { lg: 24, md: 24 },
+    },
+    {
+      label: t('请求数据'),
+      field: 'requestParams',
+      component: 'Input',
+      colProps: { lg: 24, md: 24 },
+    },
+    {
+      label: t('操作用户'),
+      field: 'createByName',
+      component: 'Input',
+      componentProps: {
+        maxlength: 100,
+      },
+      required: true,
+    },
+    {
+      label: t('操作账号'),
+      field: 'createBy',
+      component: 'Input',
+      componentProps: {
+        maxlength: 100,
+      },
+      required: true,
+    },
+    {
+      label: t('业务类型'),
+      field: 'bizType',
+      component: 'Input',
+      componentProps: {
+        maxlength: 64,
+      },
+    },
+    {
+      label: t('业务主键'),
+      field: 'bizKey',
+      component: 'Input',
+      componentProps: {
+        maxlength: 64,
+      },
+    },
+    {
+      label: t('操作时间'),
+      field: 'createDate',
+      component: 'Input',
+      componentProps: {
+        maxlength: 64,
+      },
+    },
+    {
+      label: t('客户端IP'),
+      field: 'remoteAddr',
+      component: 'Input',
+      componentProps: {
+        maxlength: 255,
+      },
+    },
+    {
+      label: t('用户代理'),
+      field: 'userAgent',
+      component: 'Input',
+      componentProps: {
+        maxlength: 500,
+      },
+      colProps: { lg: 24, md: 24 },
+    },
+    {
+      label: t('设备名称'),
+      field: 'deviceName',
+      component: 'Input',
+      componentProps: {
+        maxlength: 100,
+      },
+    },
+    {
+      label: t('浏览器名'),
+      field: 'browserName',
+      component: 'Input',
+      componentProps: {
+        maxlength: 100,
+      },
+    },
+    {
+      label: t('响应时间'),
+      field: 'executeTime',
+      component: 'Input',
+      componentProps: {
+        maxlength: 19,
+        // style: 'width: calc(50% - 60px)',
+      },
+      // colProps: { lg: 24, md: 24 },
+    },
+    {
+      label: t('差异修改数据'),
+      field: 'diffModifyDataTitle',
+      component: 'FormGroup',
+      colProps: { lg: 24, md: 24 },
+      ifShow: () => !isEmpty(record.value.diffModifyData),
+    },
+    {
+      field: 'diffModifyData',
+      component: 'Input',
+      colProps: { lg: 24, md: 24 },
+      render: () => {
+        return h('div', { innerHTML: record.value.diffModifyData });
+      },
+      ifShow: () => !isEmpty(record.value.diffModifyData),
+    },
+    {
+      label: t('异常信息'),
+      field: 'exceptionInfoTitle',
+      component: 'FormGroup',
+      colProps: { lg: 24, md: 24 },
+      ifShow: () => record.value.isException == '1',
+    },
+    {
+      field: 'exceptionInfo',
+      component: 'InputTextArea',
+      componentProps: {
+        rows: 10,
+      },
+      colProps: { lg: 24, md: 24 },
+      ifShow: () => record.value.isException == '1',
+    },
+  ];
+
+  const [registerForm, { resetFields, setFieldsValue }] = useForm({
+    labelWidth: 120,
+    schemas: inputFormSchemas,
+    baseColProps: { lg: 12, md: 24 },
+  });
+
+  const [registerDrawer, { setDrawerProps }] = useDrawerInner(async (data) => {
+    resetFields();
+    setDrawerProps({ loading: true });
+    const res = await logForm(data);
+    record.value = (res.log || {}) as Log;
+    record.value.__t = new Date().getTime();
+    record.value.requestUriFormat =
+      '[' + record.value.requestMethod + '] ' + record.value.serverAddr + record.value.requestUri;
+    setFieldsValue(record.value);
+    setDrawerProps({ loading: false });
+  });
+</script>
+<style lang="less">
+  table.table {
+    td {
+      width: 30%;
+    }
+    th:first-child,
+    table.table {
+      td:first-child {
+        width: 10%;
+      }
+    }
+  }
+</style>
---
+++ src/views/sys/log/list.vue
@@ -0,0 +1,227 @@
+<!--
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ * @author ThinkGem
+-->
+<template>
+  <div>
+    <BasicTable @register="registerTable">
+      <template #tableTitle>
+        <Icon :icon="getTitle.icon" class="m-1 pr-1" />
+        <span> {{ getTitle.value }} </span>
+      </template>
+      <template #firstColumn="{ record }">
+        <a @click="handleForm({ id: record.id })">
+          {{ record.logTitle }}
+        </a>
+      </template>
+    </BasicTable>
+    <InputForm @register="registerDrawer" @success="handleSuccess" />
+  </div>
+</template>
+<script lang="ts">
+  export default defineComponent({
+    name: 'ViewsSysLogList',
+  });
+</script>
+<script lang="ts" setup>
+  import { defineComponent, h } from 'vue';
+  import { useI18n } from '/@/hooks/web/useI18n';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import { router } from '/@/router';
+  import { Icon } from '/@/components/Icon';
+  import { BasicTable, BasicColumn, useTable } from '/@/components/Table';
+  import { logListData } from '/@/api/sys/log';
+  import { officeTreeData } from '/@/api/sys/office';
+  import { useDrawer } from '/@/components/Drawer';
+  import { FormProps } from '/@/components/Form';
+  import InputForm from './form.vue';
+
+  const { t } = useI18n('sys.log');
+  const { showMessage } = useMessage();
+  const getTitle = {
+    icon: router.currentRoute.value.meta.icon || 'ant-design:book-outlined',
+    value: router.currentRoute.value.meta.title || t('日志管理'),
+  };
+
+  const searchForm: FormProps = {
+    baseColProps: { lg: 6, md: 8 },
+    labelWidth: 90,
+    schemas: [
+      {
+        label: t('日志标题'),
+        field: 'logTitle',
+        component: 'Input',
+      },
+      {
+        label: t('请求地址'),
+        field: 'requestUri',
+        component: 'Input',
+      },
+      {
+        label: t('日志类型'),
+        field: 'logType',
+        component: 'Select',
+        componentProps: {
+          dictType: 'sys_log_type',
+          allowClear: true,
+        },
+      },
+      {
+        label: t('是否异常'),
+        field: 'isException',
+        component: 'Select',
+        componentProps: {
+          dictType: 'sys_yes_no',
+          allowClear: true,
+        },
+      },
+      {
+        label: t('操作用户'),
+        field: 'createBy',
+        component: 'TreeSelect',
+        componentProps: {
+          api: officeTreeData,
+          params: { isLoadUser: true, userIdPrefix: '' },
+          canSelectParent: false,
+          allowClear: true,
+        },
+      },
+      {
+        label: t('业务类型'),
+        field: 'bizType',
+        component: 'Input',
+      },
+      {
+        label: t('业务主键'),
+        field: 'bizKey',
+        component: 'Input',
+      },
+      {
+        label: t('操作时间'),
+        field: 'dateRange',
+        component: 'RangePicker',
+        componentProps: {},
+      },
+    ],
+    fieldMapToTime: [['dateRange', ['createDate_gte', 'createDate_lte']]],
+  };
+
+  const tableColumns: BasicColumn[] = [
+    {
+      title: t('日志标题'),
+      dataIndex: 'logTitle',
+      key: 'a.log_title',
+      sorter: true,
+      width: 180,
+      align: 'left',
+      slots: { customRender: 'firstColumn' },
+    },
+    {
+      title: t('请求地址'),
+      dataIndex: 'requestUri',
+      key: 'a.request_uri',
+      sorter: true,
+      width: 150,
+      align: 'left',
+      customRender: ({ record }) => {
+        const t = '[' + record.requestMethod + '] ' + record.serverAddr + record.requestUri;
+        return h('span', { title: t }, record.requestUri);
+      },
+    },
+    {
+      title: t('日志类型'),
+      dataIndex: 'logType',
+      key: 'a.log_type',
+      sorter: true,
+      width: 100,
+      align: 'center',
+      dictType: 'sys_log_type',
+    },
+    {
+      title: t('操作用户'),
+      dataIndex: 'createByName',
+      key: 'a.create_by_name',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('业务类型'),
+      dataIndex: 'bizType',
+      key: 'a.biz_type',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('业务主键'),
+      dataIndex: 'bizKey',
+      key: 'a.biz_key',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('客户端IP'),
+      dataIndex: 'remoteAddr',
+      key: 'a.remote_addr',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('操作时间'),
+      dataIndex: 'createDate',
+      key: 'a.create_date',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('设备名称'),
+      dataIndex: 'deviceName',
+      key: 'a.device_name',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('浏览器名'),
+      dataIndex: 'browserName',
+      key: 'a.browser_name',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: t('响应时间'),
+      dataIndex: 'executeTimeFormat',
+      key: 'a.execute_time',
+      sorter: true,
+      width: 100,
+      align: 'center',
+    },
+  ];
+
+  const [registerDrawer, { openDrawer }] = useDrawer();
+  const [registerTable, { reload }] = useTable({
+    api: logListData,
+    beforeFetch: (params) => {
+      return params;
+    },
+    columns: tableColumns,
+    formConfig: searchForm,
+    showTableSetting: true,
+    useSearchForm: true,
+    canResize: true,
+  });
+
+  function handleForm(record: Recordable) {
+    openDrawer(true, record);
+  }
+
+  function handleSuccess() {
+    reload();
+  }
+</script>
