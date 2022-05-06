be61c1e154c47cc29b49bd75de89dde1a30d620c
thinkgem <thinkgem@163.com>
2022-05-06 08:41:15 +0800

    default title

--- src/views/sys/empUser/form.vue
+++ src/views/sys/empUser/form.vue
@@ -59,7 +59,7 @@
   const record = ref<EmpUser>({} as EmpUser);
   const getTitle = computed(() => ({
     icon: router.currentRoute.value.meta.icon || 'ant-design:book-outlined',
-    value: record.value.isNewRecord ? t('新增用户') : t('编辑用户'),
+    value: record.value.isNewRecord ? t('新增字典') : t('编辑字典'),
   }));
   const ctrlPermi = ref<String>('');
   const op = ref<String>('');
