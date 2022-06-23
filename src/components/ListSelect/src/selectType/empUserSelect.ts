e0d61e48b6836a2f66d3846d2ea412cf07988c19
thinkgem <thinkgem@163.com>
2022-06-23 13:46:54 +0800

    员工选择列表组件配置参数

---
+++ src/components/ListSelect/src/selectType/empUserSelect.ts
@@ -0,0 +1,157 @@
+import { useI18n } from '/@/hooks/web/useI18n';
+import { BasicColumn, BasicTableProps, FormProps } from '/@/components/Table';
+import { officeTreeData } from '/@/api/sys/office';
+import { companyTreeData } from '/@/api/sys/company';
+import { empUserListData } from '/@/api/sys/empUser';
+
+const { t } = useI18n('sys.empUser');
+
+const searchForm: FormProps = {
+  baseColProps: { lg: 6, md: 8 },
+  labelWidth: 60,
+  schemas: [
+    {
+      label: t('机构'),
+      field: 'employee.office.officeCode',
+      component: 'TreeSelect',
+      componentProps: {
+        api: officeTreeData,
+      },
+    },
+    {
+      label: t('账号'),
+      field: 'loginCode',
+      component: 'Input',
+    },
+    {
+      label: t('昵称'),
+      field: 'userName',
+      component: 'Input',
+    },
+    {
+      label: t('状态'),
+      field: 'status',
+      component: 'Select',
+      componentProps: {
+        dictType: 'sys_user_status',
+        allowClear: true,
+      },
+    },
+    {
+      label: t('公司'),
+      field: 'employee.company.companyCode',
+      component: 'TreeSelect',
+      componentProps: {
+        api: companyTreeData,
+      },
+    },
+    {
+      label: t('姓名'),
+      field: 'refName',
+      component: 'Input',
+    },
+    {
+      label: t('手机'),
+      field: 'mobile',
+      component: 'Input',
+    },
+    {
+      label: t('邮箱'),
+      field: 'email',
+      component: 'Input',
+    },
+    {
+      label: t('电话'),
+      field: 'phone',
+      component: 'Input',
+    },
+  ],
+};
+
+const tableColumns: BasicColumn[] = [
+  {
+    title: t('登录账号'),
+    dataIndex: 'loginCode',
+    key: 'a.login_code',
+    sorter: true,
+    width: 100,
+    slots: { customRender: 'firstColumn' },
+  },
+  {
+    title: t('用户昵称'),
+    dataIndex: 'userName',
+    key: 'a.user_name',
+    sorter: true,
+    width: 100,
+  },
+  {
+    title: t('员工姓名'),
+    dataIndex: 'refName',
+    key: 'a.ref_name',
+    sorter: true,
+    width: 100,
+  },
+  {
+    title: t('归属机构'),
+    dataIndex: 'employee.office.officeName',
+    key: 'o.office_name',
+    sorter: true,
+    width: 100,
+  },
+  {
+    title: t('归属公司'),
+    dataIndex: 'employee.company.companyName',
+    key: 'c.company_name',
+    sorter: true,
+    width: 100,
+  },
+  {
+    title: t('状态'),
+    dataIndex: 'status',
+    key: 'a.status',
+    sorter: true,
+    width: 80,
+    dictType: 'sys_status',
+  },
+  {
+    title: t('更新时间'),
+    dataIndex: 'updateDate',
+    key: 'a.update_date',
+    sorter: true,
+    width: 130,
+  },
+  {
+    title: t('电子邮箱'),
+    dataIndex: 'email',
+    key: 'a.email',
+    sorter: true,
+    width: 130,
+  },
+  {
+    title: t('手机号码'),
+    dataIndex: 'mobile',
+    key: 'a.mobile',
+    sorter: true,
+    width: 130,
+  },
+  {
+    title: t('办公电话'),
+    dataIndex: 'phone',
+    key: 'a.phone',
+    sorter: true,
+    width: 130,
+  },
+];
+
+const tableProps: BasicTableProps = {
+  api: empUserListData,
+  columns: tableColumns,
+  formConfig: searchForm,
+  rowKey: 'userCode',
+};
+
+export default {
+  tableProps,
+  itemCode: 'userCode',
+  itemName: 'userName',
+};
