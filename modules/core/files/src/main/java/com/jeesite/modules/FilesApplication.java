f51f787813f54f17e3840055b3c9a4d58ee48332
thinkgem <thinkgem@163.com>
2023-07-07 21:34:34 +0800

    消息模块和文件模块独立微服务、扩展模块独立微服务

---
+++ modules/core/files/src/main/java/com/jeesite/modules/FilesApplication.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ */
+package com.jeesite.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Application
+ * @author ThinkGem
+ * @version 2018-10-13
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients({"com.jeesite.modules"})
+public class FilesApplication extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FilesApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		this.setRegisterErrorPageFilter(false); // 错误页面有容器来处理，而不是SpringBoot
+		return builder.sources(FilesApplication.class);
+	}
+	
+}
