f979da4f0798a64133a4fd188c32a26bbc8c733f
thinkgem <thinkgem@163.com>
2023-07-27 14:08:48 +0800

    remove locale-provider

--- types/module.d.ts
+++ types/module.d.ts
@@ -4,11 +4,11 @@ declare module '*.vue' {
   export default Component;
 }
 
-declare module 'ant-design-vue/es/locale/*' {
-  import { Locale } from 'ant-design-vue/types/locale-provider';
-  const locale: Locale & ReadonlyRecordable;
-  export default locale as Locale & ReadonlyRecordable;
-}
+// declare module 'ant-design-vue/es/locale/*' {
+//   import { Locale } from 'ant-design-vue/types/locale-provider';
+//   const locale: Locale & ReadonlyRecordable;
+//   export default locale as Locale & ReadonlyRecordable;
+// }
 
 declare module 'virtual:*' {
   const result: any;
