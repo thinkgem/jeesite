31e6b01b4eca6e006ce9359f7e780db2b74a1314
thinkgem <thinkgem@163.com>
2023-10-26 11:27:50 +0800

    新增一个日志清理接口，可根据需要创建job调用

--- modules/core/src/main/java/com/jeesite/modules/sys/service/LogService.java
+++ modules/core/src/main/java/com/jeesite/modules/sys/service/LogService.java
@@ -25,5 +25,13 @@ public interface LogService extends CrudServiceApi<Log> {
 	 * 不使用数据库事务，执行插入日志
 	 */
 	void insertLog(Log entity);
-	
+
+	/**
+	 * 清理指定日期之前的日志（可新建job定时调用）
+	 * 1、清理1年前的所有日志：logService.deleteLogBefore(1, 0, 0);
+	 * 2、清理6个月前的所有日志：logService.deleteLogBefore(0, 6, 0);
+	 * 3、清理7天前的所有日志：logService.deleteLogBefore(0, 0, 7);
+	 * 4、清理1年6个月前的所有日志：logService.deleteLogBefore(1, 6, 0);
+	 */
+	void deleteLogBefore(Integer year, Integer months, Integer days);
 }
