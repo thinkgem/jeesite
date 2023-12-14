2ad85b2a5b371a5632b511c9976dc1f5da8bfedc
thinkgem <thinkgem@163.com>
2023-12-14 15:24:21 +0800

    代码优化

--- src/components/Upload/src/UploadModal.vue
+++ src/components/Upload/src/UploadModal.vue
@@ -148,7 +148,7 @@
 
       function addFileItem(record: FileItem) {
         const { maxNumber } = props;
-        if ((fileItemList.value.length + props.previewFileList?.length ?? 0) >= maxNumber) {
+        if ((fileItemList.value.length + (props.previewFileList?.length || 0)) >= maxNumber) {
           createMessage.warning(t('component.upload.maxNumber', [maxNumber]));
           return;
         }
