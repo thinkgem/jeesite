82fbcbe094c86f0ffd080b9c93faa180e1816165
thinkgem <thinkgem@163.com>
2024-01-09 10:22:11 +0800

    本地缓存超期时间修改默认为60天

--- src/settings/encryptionSetting.ts
+++ src/settings/encryptionSetting.ts
@@ -1,9 +1,9 @@
 import { isDevMode } from '/@/utils/env';
 
-// System default cache time, in seconds
-export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 7;
+// System default cache time, in seconds ( 60 days )
+export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 60;
 
-// aes encryption key
+// aes encryption key（key and iv 16 bits）
 export const cacheCipher = {
   key: '_11111000001111@',
   iv: '@11111000001111_',
