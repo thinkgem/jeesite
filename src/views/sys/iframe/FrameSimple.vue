6ec0d428e05a616acd588550f3f8ece750c32e83
thinkgem <thinkgem@163.com>
2024-01-09 17:02:26 +0800

    新增frame单一组件，用来不显示框架页时的路由展示外部网页

---
+++ src/views/sys/iframe/FrameSimple.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="showFrame">
+    <template v-for="frame in getFramePages" :key="frame.path">
+      <template v-if="frame.meta.frameSrc && showIframe(frame)">
+        <FramePage :frame="frame" :fullHeight="true" />
+      </template>
+    </template>
+  </div>
+</template>
+<script lang="ts">
+  import { computed, defineComponent, unref } from 'vue';
+  import FramePage from '/@/views/sys/iframe/index.vue';
+  import { useFrameKeepAlive } from '/@/layouts/iframe/useFrameKeepAlive';
+
+  export default defineComponent({
+    name: 'FrameSimple',
+    components: { FramePage },
+    setup() {
+      const { getFramePages, hasRenderFrame, showIframe } = useFrameKeepAlive();
+      const showFrame = computed(() => unref(getFramePages).length > 0);
+      return { getFramePages, hasRenderFrame, showIframe, showFrame };
+    },
+  });
+</script>
