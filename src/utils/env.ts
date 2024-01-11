a8236dc9125b830b0fcb40f802f29d5926b7f373
thinkgem <thinkgem@163.com>
2024-01-11 13:33:18 +0800

    优化 env 和 publicPath 属性获取

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,6 @@
 import type { GlobEnvConfig } from '/#/config';
 
-import { warn } from '/@/utils/log';
+import { warn, env } from '/@/utils/log';
 import { version } from '../../package.json';
 import { getConfigFileName } from '../../build/getConfigFileName';
 
@@ -15,34 +15,19 @@ export function getStorageShortName() {
 }
 
 export function getAppEnvConfig() {
-  const ENV_NAME = getConfigFileName(import.meta.env);
-
-  const ENV = (import.meta.env.DEV
+  const ENV_NAME = getConfigFileName(env);
+  const ENV = (env.DEV
     ? // Get the global configuration (the configuration will be extracted independently when packaging)
-      (import.meta.env as unknown as GlobEnvConfig)
+      (env as unknown as GlobEnvConfig)
     : window[ENV_NAME as any]) as unknown as GlobEnvConfig;
 
-  const {
-    VITE_GLOB_APP_TITLE,
-    VITE_GLOB_API_URL,
-    VITE_GLOB_APP_SHORT_NAME,
-    VITE_GLOB_API_URL_PREFIX,
-    // VITE_GLOB_UPLOAD_URL,
-  } = ENV;
-
-  if (!/^[a-zA-Z\_]*$/.test(VITE_GLOB_APP_SHORT_NAME)) {
+  if (!/^[a-zA-Z\_]*$/.test(ENV.VITE_GLOB_APP_SHORT_NAME)) {
     warn(
       `VITE_GLOB_APP_SHORT_NAME Variables can only be characters/underscores, please modify in the environment variables and re-running.`,
     );
   }
 
-  return {
-    VITE_GLOB_APP_TITLE,
-    VITE_GLOB_API_URL,
-    VITE_GLOB_APP_SHORT_NAME,
-    VITE_GLOB_API_URL_PREFIX,
-    // VITE_GLOB_UPLOAD_URL,
-  };
+  return ENV;
 }
 
 /**
@@ -56,28 +41,32 @@ export const devMode = 'development';
 export const prodMode = 'production';
 
 /**
- * @description: Get environment variables
- * @returns:
- * @example:
+ * @description: Get environment mode
  */
 export function getEnv(): string {
-  return import.meta.env.MODE;
+  return env.MODE;
 }
 
 /**
  * @description: Is it a development mode
- * @returns:
- * @example:
  */
 export function isDevMode(): boolean {
-  return import.meta.env.DEV;
+  return env.DEV;
 }
 
 /**
  * @description: Is it a production mode
- * @returns:
- * @example:
  */
 export function isProdMode(): boolean {
-  return import.meta.env.PROD;
+  return env.PROD;
 }
+
+/**
+ * @description: VITE_PUBLIC_PATH
+ */
+function getPublicPath(): string {
+  const publicPath = env.VITE_PUBLIC_PATH || '';
+  return publicPath == '/' ? '' : publicPath;
+}
+
+export const publicPath = getPublicPath();
