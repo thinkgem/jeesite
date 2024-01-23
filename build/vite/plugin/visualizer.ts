f4263f188b5ebdb8849414685815401197e57116
thinkgem <thinkgem@163.com>
2024-01-23 12:10:01 +0800

    update visualizer ts type check

--- build/vite/plugin/visualizer.ts
+++ build/vite/plugin/visualizer.ts
@@ -1,18 +1,18 @@
 /**
  * Package file volume analysis
  */
-import { Plugin } from 'vite';
 import visualizer from 'rollup-plugin-visualizer';
 import { isReportMode } from '../../utils';
+import { PluginOption } from 'vite';
 
-export function configVisualizerConfig() {
+export function configVisualizerPlugin() {
   if (isReportMode()) {
-    return visualizer({
+    return (visualizer as any).default({
       filename: './node_modules/.cache/visualizer/stats.html',
       open: true,
       gzipSize: true,
       brotliSize: true,
-    }) as Plugin;
+    }) as PluginOption;
   }
   return [];
 }
