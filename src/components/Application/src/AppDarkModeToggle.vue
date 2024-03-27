73f94eda57eb92de184fd4279c1e25ffb7e3d1ca
thinkgem <thinkgem@163.com>
2024-03-27 11:46:31 +0800

    <SvgIcon name="sun" to <Icon icon-"i-svg:sun"

--- src/components/Application/src/AppDarkModeToggle.vue
+++ src/components/Application/src/AppDarkModeToggle.vue
@@ -1,13 +1,13 @@
 <template>
   <div v-if="getShowDarkModeToggle" :class="getClass" @click="toggleDarkMode">
     <div :class="`${prefixCls}-inner`"></div>
-    <SvgIcon size="14" name="sun" />
-    <SvgIcon size="14" name="moon" />
+    <Icon icon="i-svg:sun" size="14" />
+    <Icon icon="i-svg:moon" size="14" />
   </div>
 </template>
 <script lang="ts" setup>
   import { computed, unref } from 'vue';
-  import { SvgIcon } from '/@/components/Icon';
+  import { Icon } from '/@/components/Icon';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { useRootSetting } from '/@/hooks/setting/useRootSetting';
   import { updateHeaderBgColor, updateSidebarBgColor } from '/@/logics/theme/updateBackground';
