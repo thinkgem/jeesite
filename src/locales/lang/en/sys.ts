3f9011ec4ac9dedf6f51a9d8a0773e4fa31804b8
thinkgem <thinkgem@163.com>
2024-04-12 21:45:19 +0800

    如果填写组件路径，则组件名称根据组件路径自动生成，而不是链接地址

--- src/locales/lang/en/sys.ts
+++ src/locales/lang/en/sys.ts
@@ -1,7 +1,7 @@
 export default {
   api: {
     operationFailed: 'Operation failed',
-    errorTip: 'Error Tip',
+    errorTip: 'System Tip',
     errorMessage: 'The operation failed, the system is abnormal!',
     timeoutMessage: 'Login timed out, please log in again!',
     apiTimeoutMessage: 'The interface request timed out, please refresh the page and try again!',
