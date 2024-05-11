d0eba614bd8163201656c2b7ed1d957d8a55dd2c
thinkgem <thinkgem@163.com>
2024-05-11 13:44:05 +0800

    调整配置文件目录

--- modules/core/src/main/java/com/jeesite/modules/config/interceptor/LogInterceptorConfig.java
+++ modules/core/src/main/java/com/jeesite/modules/config/interceptor/LogInterceptorConfig.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
  * No deletion without permission, or be held responsible to law.
  */
-package com.jeesite.modules.config.web.interceptor;
+package com.jeesite.modules.config.interceptor;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
@@ -13,27 +13,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.jeesite.common.config.Global;
 import com.jeesite.common.lang.StringUtils;
-import com.jeesite.modules.sys.interceptor.MobileInterceptor;
+import com.jeesite.modules.sys.interceptor.LogInterceptor;
 
 /**
- * 前台自动切换到手机视图拦截器
+ * 后台管理日志记录拦截器
  * @author ThinkGem
  * @version 2018年1月10日
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnProperty(name="web.interceptor.mobile.enabled", havingValue="true", matchIfMissing=false)
+@ConditionalOnProperty(name="web.interceptor.log.enabled", havingValue="true", matchIfMissing=true)
 @EnableWebMvc
-public class MobileViewInterceptorConfig implements WebMvcConfigurer {
+public class LogInterceptorConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		InterceptorRegistration registration = registry.addInterceptor(new MobileInterceptor());
-		for (String uri : StringUtils.splitComma(Global.getProperty("web.interceptor.mobile.addPathPatterns"))){
+		InterceptorRegistration registration = registry.addInterceptor(new LogInterceptor());
+		for (String uri : StringUtils.splitComma(Global.getProperty("web.interceptor.log.addPathPatterns"))){
 			if (StringUtils.isNotBlank(uri)){
 				registration.addPathPatterns(StringUtils.trim(uri));
 			}
 		}
-		for (String uri : StringUtils.splitComma(Global.getProperty("web.interceptor.mobile.excludePathPatterns"))){
+		for (String uri : StringUtils.splitComma(Global.getProperty("web.interceptor.log.excludePathPatterns"))){
 			if (StringUtils.isNotBlank(uri)){
 				registration.excludePathPatterns(StringUtils.trim(uri));
 			}
--- modules/core/src/main/java/com/jeesite/modules/config/server/SchemeHttpsConfig.java
+++ modules/core/src/main/java/com/jeesite/modules/config/server/SchemeHttpsConfig.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
  * No deletion without permission, or be held responsible to law.
  */
-package com.jeesite.modules.config.web;
+package com.jeesite.modules.config.server;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
