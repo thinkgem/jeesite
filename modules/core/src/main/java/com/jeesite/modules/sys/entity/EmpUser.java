a15fb508f9477074e4bde0dd3d48a06895b63839
thinkgem <thinkgem@163.com>
2024-07-23 19:39:56 +0800

    新增 EncryptTypeHandler、SM4TypeHandler 加密 MyBatis Handler

--- modules/core/src/main/java/com/jeesite/modules/sys/entity/EmpUser.java
+++ modules/core/src/main/java/com/jeesite/modules/sys/entity/EmpUser.java
@@ -29,7 +29,7 @@ import jakarta.validation.Valid;
 		// 手机号加密脱敏例子，共同打开 EmpUserDao.java 和 EmpUserDao.xml 中的 result 注释
 		// 详细 typeHandler 用法，请看文档：https://jeesite.com/docs/dao-mybatis/#手机号加密脱敏
 //		@Column(name="mobile", attrName="mobile", label="手机号码", queryType=QueryType.EQ,
-//			javaType = String.class, typeHandler = AesTypeHandler.class),
+//			javaType = String.class, typeHandler = EncryptTypeHandler.class),
 		@Column(includeEntity=User.class),
 	}, joinTable={
 		@JoinTable(type=Type.JOIN, entity=Employee.class, alias="e",
