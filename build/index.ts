1c399881cdf86b12463d1e100749c9bcb9b518c7
thinkgem <thinkgem@163.com>
2024-08-20 16:25:37 +0800

    build add index.ts

---
+++ build/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ * @author ThinkGem
+ */
+export * from './config';
+export * from './options';
+export * from './plugins';
---
+++ build/options/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ * @author ThinkGem
+ */
+export * from './build';
+export * from './css';
+export * from './define';
+export * from './esBuild';
+export * from './server';
+export * from './index';
