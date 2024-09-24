2904fbc081e30727829a210d76d3b46b4161be00
thinkgem <thinkgem@163.com>
2024-09-24 17:11:37 +0800

    lint

--- src/views/sys/login/QrCodeForm.vue
+++ src/views/sys/login/QrCodeForm.vue
@@ -3,17 +3,10 @@
     <LoginFormTitle class="enter-x" />
     <!-- <div class="gp mt-4 mb-2"> Tip：演示系统未开放扫码登录，请联系官方人员获取账号密码。 </div> -->
     <div class="enter-x min-h-64 min-w-64">
-      <!-- <QrCode
+      <QrCode
         :value="qrCodeUrl"
         class="enter-x flex justify-center xl:justify-start"
         :width="280"
-      /> -->
-      <Image
-        :src="pictureUrl"
-        class="enter-x flex cursor-pointer justify-center xl:justify-start"
-        :width="280"
-        :preview="false"
-        @click="qrcodeGen"
       />
       <Divider class="enter-x size">{{ t('请使用微信扫描二维码，点击图片刷新！') }}</Divider>
       <Button size="large" block class="enter-x mt-4" @click="handleBackLogin">
@@ -26,11 +19,11 @@
   import { computed, unref } from 'vue';
   import LoginFormTitle from './LoginFormTitle.vue';
   import { Button, Divider } from 'ant-design-vue';
-  // import { QrCode } from '/@/components/Qrcode';
+  import { QrCode } from '/@/components/Qrcode';
   import { useI18n } from '/@/hooks/web/useI18n';
   import { useLoginState, LoginStateEnum } from './useLogin';
 
-  const qrCodeUrl = 'http://jeesite.com';
+  const qrCodeUrl = 'https://jeesite.com';
 
   const { t } = useI18n();
   const { handleBackLogin, getLoginState } = useLoginState();
