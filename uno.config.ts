c2717d64d4505dbc38b9fd793a75dfac09dbea4e
thinkgem <thinkgem@163.com>
2025-01-07 14:33:32 +0800

    update scan path

--- uno.config.ts
+++ uno.config.ts
@@ -10,8 +10,8 @@ import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders';
 export default defineConfig({
   content: {
     pipeline: {
-      include: ['**/*.vue', '**/*.tsx', '**/*.ts', '**/assets/*.js'],
-      exclude: ['.git', '.idea', '.turbo', 'dist', 'node_modules', 'public'],
+      include: ['**/*.vue', '**/*.tsx', '**/*.ts', '**/@jeesite/**/*.js'],
+      exclude: ['.git', '.idea', '.turbo', 'node_modules', 'public'],
     },
   },
   presets: [
