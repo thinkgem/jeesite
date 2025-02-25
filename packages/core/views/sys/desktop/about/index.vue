9856b8041817f31ef10e0adf45e33044347dc811
thinkgem <thinkgem@163.com>
2025-02-25 10:28:08 +0800

    update about.vue

--- packages/core/views/sys/desktop/about/index.vue
+++ packages/core/views/sys/desktop/about/index.vue
@@ -6,12 +6,12 @@
           <a href="https://jeesite.com" target="_blank">JeeSite</a>
           快速开发平台，不仅仅是一个后台开发框架，它是一个企业级快速开发解决方案，有平台来封装技术细节，
           让开发者更专注业务，降低软件的开发难度。平台基于经典组合 Spring Boot、Apache MyBatis，
-          前端采用：Vue3、Vite、Ant-Design-Vue、TypeScript、
+          前端采用：Vue3、Vite、Monorepo、Ant-Design-Vue、TypeScript、
           <a href="https://github.com/anncwb/vue-vben-admin" target="_blank">Vue Vben Admin</a>，
           最先进的技术栈，让初学者能够更快的入门并投入到团队开发中去。
           提供在线代码生成功能，包括模块如：组织机构、角色用户、菜单及按钮授权、数据权限、系统参数、内容管理、工作流等。
-          众多账号安全设置，密码策略；文件在线预览；消息推送；
-          多元化第三方登录；在线定时任务配置；支持集群，支持SAAS；支持多数据源；支持读写分离、分库分表；支持微服务应用。
+          众多账号安全设置，密码策略；文件在线预览；消息推送；多元化第三方登录；在线定时任务配置；支持集群，支持SAAS；
+          支持多数据源；支持读写分离、分库分表；支持 Spring Cloud 分布式微服务应用架构。
           强大的组件封装，数据驱动视图。为微小中大型项目的开发，提供现成的开箱解决方案及丰富的示例。
         </span>
       </div>
