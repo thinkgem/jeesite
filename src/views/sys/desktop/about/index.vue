338933aab6a59e9f4aaefba0ec57c7ca1a85c2d6
thinkgem <thinkgem@163.com>
2025-02-25 10:26:39 +0800

    update about.vue

--- src/views/sys/desktop/about/index.vue
+++ src/views/sys/desktop/about/index.vue
@@ -10,8 +10,8 @@
           <a href="https://github.com/anncwb/vue-vben-admin" target="_blank">Vue Vben Admin</a>，
           最先进的技术栈，让初学者能够更快的入门并投入到团队开发中去。
           提供在线代码生成功能，包括模块如：组织机构、角色用户、菜单及按钮授权、数据权限、系统参数、内容管理、工作流等。
-          众多账号安全设置，密码策略；文件在线预览；消息推送；
-          多元化第三方登录；在线定时任务配置；支持集群，支持SAAS；支持多数据源；支持读写分离、分库分表；支持微服务应用。
+          众多账号安全设置，密码策略；文件在线预览；消息推送；多元化第三方登录；在线定时任务配置；支持集群，支持SAAS；
+          支持多数据源；支持读写分离、分库分表；支持 Spring Cloud 分布式微服务应用架构。
           强大的组件封装，数据驱动视图。为微小中大型项目的开发，提供现成的开箱解决方案及丰富的示例。
         </span>
       </div>
@@ -24,8 +24,8 @@
 <script lang="ts" setup name="AboutPage">
   import { h } from 'vue';
   import { Tag } from 'ant-design-vue';
-  import { PageWrapper } from '/@/components/Page';
-  import { Description, DescItem, useDescription } from '/@/components/Description';
+  import { PageWrapper } from '@jeesite/core/components/Page';
+  import { Description, DescItem, useDescription } from '@jeesite/core/components/Description';
 
   const { pkg, lastBuildTime } = __APP_INFO__;
 
