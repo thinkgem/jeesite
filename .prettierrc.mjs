1e56bbe7d45d5061c63abb0f6f910e9b2f4d92cd
thinkgem <thinkgem@163.com>
2025-02-27 16:21:54 +0800

    升级 eslint 9.21、prettier、stylelint、等等

---
+++ .prettierrc.mjs
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ * @author ThinkGem
+ */
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  trailingComma: 'all',
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-packagejson'],
+  overrides: [
+    {
+      files: '.*rc',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
