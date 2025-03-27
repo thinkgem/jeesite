c7cc1343c8f1fb2f1495750118a7ed7c557f8ea9
thinkgem <thinkgem@163.com>
2025-03-27 14:15:30 +0800

    优化外部调用接口调用测试类

--- modules/core/src/test/java/com/jeesite/test/MobileAppTest.java
+++ modules/core/src/test/java/com/jeesite/test/MobileAppTest.java
@@ -1,13 +1,13 @@
 package com.jeesite.test;
 
-import java.util.List;
-import java.util.Map;
-
 import com.jeesite.common.codec.DesUtils;
 import com.jeesite.common.collect.ListUtils;
 import com.jeesite.common.collect.MapUtils;
 import com.jeesite.common.mapper.JsonMapper;
 import com.jeesite.common.web.http.HttpClientUtils;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 手机接入测试类
@@ -18,18 +18,19 @@ public class MobileAppTest {
 	
 	public static void main(String[] args) {
 
-		String url = null;
-		
 		// 获取权限信息
-		url = "/index";
+		String url = "/index";
+		System.out.println("========== 获取状态 ==========");
 		System.out.println("result: " + post(url, null));
 
 		// 获取部门列表数据
 		url = "/sys/office/treeData";
+		System.out.println("========== 部门列表 ==========");
 		System.out.println("result: " + post(url, null));
 
 		// 获取用户列表数据
 		url = "/sys/empUser/treeData";
+		System.out.println("========== 用户列表 ==========");
 		System.out.println("result: " + post(url, null));
 
 	}
@@ -37,14 +38,14 @@ public class MobileAppTest {
 	/**
 	 * 服务器地址
 	 */
-	private static String servUrl = "http://127.0.0.1:8980/js/a";
-	
+	private static final String servUrl = "http://127.0.0.1:8980/js/a";
+
 	/**
 	 * 登录地址及登录信息
 	 */
-	private static String loginUrl = "/login"
-			+ "?username=" + DesUtils.encode("system", "thinkgem,jeesite,com")
-			+ "&password=" + DesUtils.encode("admin", "thinkgem,jeesite,com")
+	private static final String loginUrl = "/login"
+			+ "?username=" + DesUtils.encode("system", "Base64")
+			+ "&password=" + DesUtils.encode("admin", "Base64")
 			+ "&param_deviceType=mobileApp&param_lang=zh_CN&__sid="; 
 	
 	/**
@@ -57,11 +58,13 @@ public class MobileAppTest {
 	 * 获取有效的会话编号
 	 */
 	public static void refreshSid(){
+		System.out.println("========== 登录系统 ==========");
 		List<Map<String, Object>> result = post(loginUrl, null);
 		if (result.size() == 1){
 			Map<String, Object> res = result.get(0);
 			if ("true".equals(res.get("result"))){
 				__sid = (String)res.get("sessionid");
+				System.out.println("========== 登录成功 ==========");
 			}else{
 				throw new RuntimeException("操作失败！错误信息：" + res.get("message"));
 			}
