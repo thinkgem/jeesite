2931cc52bb615589d05dc0bc4cbd938634ef229d
thinkgem <thinkgem@163.com>
2025-03-29 11:08:35 +0800

    新增 Tool calling，让 AI 调用你的 java 实现你的业务联动

--- modules/cms-ai/src/main/java/com/jeesite/modules/cms/ai/config/CmsAiChatConfig.java
+++ modules/cms-ai/src/main/java/com/jeesite/modules/cms/ai/config/CmsAiChatConfig.java
@@ -5,6 +5,7 @@
 package com.jeesite.modules.cms.ai.config;
 
 import com.jeesite.common.datasource.DataSourceHolder;
+import com.jeesite.modules.cms.ai.tools.CmsAiTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,31 @@ import java.sql.SQLException;
 public class CmsAiChatConfig {
 
 	/**
+	 * 聊天对话客户端
+	 * @author ThinkGem
+	 */
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder
+			.defaultSystem("""
+					## 人物设定
+					你是我的知识库AI助手，你把我当作朋友，耐心真诚地回复我提出的相关问题。
+					你需要遵循以下原则，与关注者进行友善而有价值的沟通。
+					## 表达方式：
+					1. 使用简体中文回答我的问题。
+					2. 使用幽默有趣的方式与我沟通。
+					3. 增加互动，如 “您的看法如何？”
+					""")
+			.defaultTools(new CmsAiTools())
+            .build();
+    }
+
+//	@Bean
+//	public BatchingStrategy batchingStrategy() {
+//		return new TokenCountBatchingStrategy(EncodingType.CL100K_BASE, Integer.MAX_VALUE, 0.1);
+//	}
+
+	/**
 	 * PG向量库数据源
 	 * @author ThinkGem
 	 */
@@ -34,21 +60,5 @@ public class CmsAiChatConfig {
 				.createDataSource("ds_pgvector");
 		return new JdbcTemplate(dataSource);
 	}
-
-	/**
-	 * 聊天对话客户端
-	 * @author ThinkGem
-	 */
-    @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder
-			.defaultSystem("你是我的知识库AI助手，请帮我解答我提出的相关问题。")
-            .build();
-    }
-
-//	@Bean
-//	public BatchingStrategy batchingStrategy() {
-//		return new TokenCountBatchingStrategy(EncodingType.CL100K_BASE, Integer.MAX_VALUE, 0.1);
-//	}
 
 }
