9ee2e8a00e0f7916a1c6a198ac3b902519014f86
thinkgem <thinkgem@163.com>
2025-04-14 12:39:06 +0800

    代码优化

--- src/enums/cacheEnum.ts
+++ src/enums/cacheEnum.ts
@@ -1,11 +1,12 @@
 export const TOKEN_KEY = 'TOKEN__';
-export const LOCALE_KEY = 'LOCALE__';
 export const SESSION_TIMEOUT_KEY = 'SESSION_TIMEOUT__';
 export const USER_INFO_KEY = 'USER_INFO__';
 export const ROLES_KEY = 'ROLES__';
-export const PROJ_CFG_KEY = 'PROJ_CFG__';
 export const LOCK_INFO_KEY = 'LOCK_INFO__';
+export const PROJ_CFG_KEY = 'PROJ_CFG__';
 export const MULTIPLE_TABS_KEY = 'MULTIPLE_TABS__';
+
+export const LOCALE_KEY = 'LOCALE__';
 export const APP_DARK_MODE_KEY = 'APP_DARK_MODE__';
 export const APP_LOCAL_CACHE_KEY = 'COMMON_LOCAL__';
 export const APP_SESSION_CACHE_KEY = 'COMMON_SESSION__';
