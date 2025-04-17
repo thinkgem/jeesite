ce47d7f3419d01500a9fd59145ea7d0d3e5ed569
thinkgem <thinkgem@163.com>
2025-04-17 14:00:41 +0800

    update docker-build.sh

--- web-fast/bin/docker-build.sh
+++ web-fast/bin/docker-build.sh
@@ -19,7 +19,7 @@ echo "参考下面的脚本，拷贝到 Docker 服务器上运行："
 echo ""
 echo "docker run --name jeesite-web -p 8980:8980 -d --restart unless-stopped -v ~/:/data thinkgem/jeesite-web && docker logs -f jeesite-web"
 echo ""
-echo "启动完成后，访问项目：http://127.0.0.1:8980/js/a/login   用户名：system   密码：admin"
+echo "启动完成后，访问地址：http://127.0.0.1:8980   用户名：system   密码：admin"
 echo ""
 
 cd bin
