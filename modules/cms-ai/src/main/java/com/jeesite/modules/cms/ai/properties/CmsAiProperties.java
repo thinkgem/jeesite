5fc096ab0a946c900efbc7181aea38e9ed2c230f
thinkgem <thinkgem@163.com>
2025-06-18 10:59:44 +0800

    通过yml开关控制使用哪些模型和向量库

--- modules/cms-ai/src/main/java/com/jeesite/modules/cms/ai/properties/CmsAiProperties.java
+++ modules/cms-ai/src/main/java/com/jeesite/modules/cms/ai/properties/CmsAiProperties.java
@@ -1,6 +1,7 @@
 package com.jeesite.modules.cms.ai.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 @ConfigurationProperties("spring.ai")
 public class CmsAiProperties {
@@ -19,6 +20,12 @@ public class CmsAiProperties {
 	 * 默认问题模板格式
 	 */
 	private String defaultPromptTemplate = "";
+
+	/**
+	 * 向量数据库设置
+	 */
+	@NestedConfigurationProperty
+	private final Vectorstore vectorstore = new Vectorstore();
 
 	public Boolean getToolCalls() {
 		return toolCalls;
@@ -42,5 +49,25 @@ public class CmsAiProperties {
 
 	public void setDefaultPromptTemplate(String defaultPromptTemplate) {
 		this.defaultPromptTemplate = defaultPromptTemplate;
+	}
+
+	public Vectorstore getVectorstore() {
+		return vectorstore;
+	}
+
+	public static class Vectorstore {
+
+		/**
+		 * 向量库类型选择：chroma、pgvector、elasticsearch、milvus
+		 */
+		private String type;
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
 	}
 }
