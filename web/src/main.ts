54c40718b99456b839fc814016c4c4e040f6c91c
thinkgem <thinkgem@163.com>
2025-09-22 23:32:24 +0800

    update

--- web/src/main.ts
+++ web/src/main.ts
@@ -60,7 +60,7 @@ async function bootstrap() {
 // 仅开发模式显示
 if (!isDevMode()) {
   console.log(
-    '%c JeeSite %cVue \n%c 用心去做我们的快速开发平台，用心去帮助我们的客户！好不好用，您说的算。\n 您的一个关注，就是对我们最大的支持：  https://gitee.com/thinkgem/jeesite-vue  （请点 star 收藏我们）\n 免费技术 QQ 交流群： 127515876、209330483、223507718、709534275、730390092、1373527、183903863(外包) %c\n ',
+    '%c JeeSite %c快速开发平台 \n%c 用心去做我们的快速开发平台，用心去帮助我们的客户！让您用着省心的平台。\n 您的一个关注，就是对我们最大的支持：  https://gitee.com/thinkgem/jeesite-vue  （请点 star 收藏我们）\n 免费 QQ 技术交流群： 127515876、209330483、223507718、709534275、730390092、1373527、183903863(外包) \n 免费 微信 技术交流群： http://s.jeesite.com 如果加不上，可添加 微信 jeesitex 邀请您进群。%c\n ',
     'font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size:39px;color:#0f87e8;-webkit-text-fill-color:#0f87e8;-webkit-text-stroke:1px #0f87e8;',
     'font-size:24px;color:#aaa;',
     'font-size:14px;color:#888;',
