f2182b255711af73cf15a9e4046b32f2e70c6399
thinkgem <thinkgem@163.com>
2025-09-30 20:03:20 +0800

    优化 FeignClient 生成的 Bean 名称，直接使用 类名 小写开头

--- modules/core/files/src/main/java/com/jeesite/modules/FilesApplication.java
+++ modules/core/files/src/main/java/com/jeesite/modules/FilesApplication.java
@@ -4,12 +4,14 @@
  */
 package com.jeesite.modules;
 
+import com.jeesite.modules.cloud.feign.EnableFeignClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Application
@@ -19,9 +21,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients({"com.jeesite.modules"})
 public class FilesApplication extends SpringBootServletInitializer {
+
+	private static final Logger logger = LoggerFactory.getLogger(FilesApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FilesApplication.class, args);
+		logger.info(
+				"\r\n\r\n==============================================================\r\n"
+				+ "\r\n   " + FilesApplication.class.getName() + " 启动完成。"
+				+ "\r\n\r\n==============================================================\r\n");
 	}
 	
 	@Override
