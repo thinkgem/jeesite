c32eb73d7327253b239dd3ea525f7b8ae9cb04da
thinkgem <thinkgem@163.com>
2025-10-17 22:27:08 +0800

    update cms chat

--- packages/cms/views/cms/chat/index.vue
+++ packages/cms/views/cms/chat/index.vue
@@ -7,14 +7,14 @@
   <PageWrapper :sidebarWidth="230" :contentFullHeight="true">
     <template #sidebar>
       <div class="p-2 pt-1">
-        <a-button type="primary" class="w-full" @click="handleAdd">
+        <a-button type="primary" class="w-full" @click="handleAdd" :disabled="loading">
           <Icon icon="i-ant-design:plus-outlined" /> {{ t('新建对话') }}
         </a-button>
       </div>
-      <ScrollContainer class="p-2 bg-white rounded-2 h-full">
-        <Menu style="border-right: 0; padding: 2px" v-model:selectedKeys="conversationIds">
+      <ScrollContainer class="jeesite-cms-ai p-2 bg-white rounded-2 h-full">
+        <Menu class="jeesite-cms-ai-menu" v-model:selectedKeys="conversationIds" :disabled="loading">
           <template v-for="(item, index) in chatList" :key="item.id">
-            <Menu.Item @click="handleSelect(item)" style="padding-right: 8px">
+            <Menu.Item @click="handleSelect(item)">
               <div class="flex justify-end">
                 <span v-if="item.edit" class="flex-1 mr-2">
                   <a-input
@@ -39,6 +39,17 @@
       </ScrollContainer>
     </template>
     <div class="h-full rounded-2 flex flex-col overflow-hidden">
+      <div v-if="messages.length == 0" class="h-[90%] flex justify-center items-center text-center">
+        <div class="text-xl c-gray-4">
+          {{ t('我是你的 AI 助手，我可以帮你解答一些问题') }}
+          <div v-if="userStore.getPageCacheByKey('demoMode')" class="text-sm mt-20 line-height-loose">
+            提示：当前对接的是 DeepSeek 蒸馏过的 7B 超小模型，仅作为演示使用，AI 回答结果可能不够理想，<br />
+            可在自己本地部署，或对接其它大模型。此外当前向量库中只含了几篇关于 jeesite 的文章，<br />
+            知识库文章来源，可进入菜单查看：扩展功能 -> 内容管理 -> 内容发布<br />
+            提问举例：jeesite 简介、jeesite 优势、jeesite 技术栈，体验一下。
+          </div>
+        </div>
+      </div>
       <ChatMessage
         ref="messageRef"
         v-model:value="messages"
@@ -46,13 +57,7 @@
         :conversationId="conversationIds[0]"
         :inputMessage="inputMessageRef?.value"
         v-model:loading="loading"
-      >
-        <template #header>
-          <div v-if="messages.length == 0" class="h-[90%] flex justify-center items-center c-gray-4 text-xl">
-            {{ t('我是你的 AI 助手，我可以帮你解答一些问题') }}
-          </div>
-        </template>
-      </ChatMessage>
+      />
       <div class="pl-14 pr-16 w-full flex justify-end mt-3">
         <div class="flex-1 rounded-2 p-3 pb-1 bg-white text-[15px] leading-7">
           <textarea
@@ -85,6 +90,7 @@
   import { Icon } from '@jeesite/core/components/Icon';
   import { useI18n } from '@jeesite/core/hooks/web/useI18n';
   import { useMessage } from '@jeesite/core/hooks/web/useMessage';
+  import { useUserStore } from '@jeesite/core/store/modules/user';
   import { PageWrapper } from '@jeesite/core/components/Page';
   import { ScrollContainer } from '@jeesite/core/components/Container';
   import { cmsChatDelete, cmsChatList, cmsChatMessage, cmsChatSave, cmsChatStream } from '@jeesite/cms/api/cms/chat';
@@ -94,12 +100,12 @@
   const { showMessage } = useMessage();
   const conversationIds = ref<string[]>([]);
   const conversationTitle = ref<string>('');
+  const userStore = useUserStore();
 
   const loading = ref(false);
   const messageRef = ref<InstanceType<typeof ChatMessage>>();
   const inputMessageRef = ref<HTMLTextAreaElement>();
   const messages = ref<Recordable[]>([]);
-
   const chatList = ref<Recordable[]>([]);
 
   onMounted(async () => {
@@ -109,7 +115,9 @@
       chatList.value.unshift(res);
     }
     await nextTick(async () => {
-      await handleSelect(chatList.value[0]);
+      setTimeout(async () => {
+        await handleSelect(chatList.value[0]);
+      }, 100);
     });
   });
 
@@ -160,26 +168,21 @@
     }
   }
 
-  function handleInputMessage(value: string) {
-    if (inputMessageRef.value) {
-      inputMessageRef.value.value = value;
-      inputMessageRef.value.style.height = 'auto';
-    }
-  }
-
   function handleEnter(event: KeyboardEvent) {
     if (event.key === 'Enter' && !event.shiftKey) {
       event.preventDefault();
-      handleSend();
+      if (!loading.value) {
+        handleSend();
+      }
     }
   }
 
   async function handleSend() {
     if (inputMessageRef.value && inputMessageRef.value.value) {
-      loading.value = true;
       if (!conversationIds.value[0]) {
         await handleAdd();
       }
+      loading.value = true;
       const params = {
         id: conversationIds.value[0],
         message: inputMessageRef.value.value,
@@ -187,6 +190,14 @@
       inputMessageRef.value.value = '';
       inputMessageRef.value.style.height = 'auto';
       try {
+        chatList.value
+          .filter((item) => item.id == params.id)
+          .forEach((item) => {
+            if (item.title.startsWith('新对话')) {
+              item.title = params.message.substring(0, 30);
+              cmsChatSave(item);
+            }
+          });
         await messageRef.value?.sendMessage(params);
       } finally {
         loading.value = false;
@@ -195,8 +206,35 @@
       showMessage(t('请填写你的问题'));
     }
   }
-
-  function handleStopLoading() {
-    loading.value = false;
-  }
 </script>
+<style lang="less">
+  .jeesite-cms-ai {
+    &-menu.ant-menu.ant-menu-light {
+      border-right: 0 !important;
+      padding: 2px !important;
+
+      .ant-menu-item {
+        padding-right: 8px;
+        color: #333 !important;
+
+        &-selected {
+          background-color: #f0f5ff !important;
+          color: #333 !important;
+        }
+      }
+    }
+  }
+
+  html[data-theme='dark'] {
+    .jeesite-cms-ai {
+      .ant-menu-light .ant-menu-item {
+        color: #ddd !important;
+
+        &-selected {
+          background-color: #333 !important;
+          color: #ddd !important;
+        }
+      }
+    }
+  }
+</style>
