b27c38c0a468866852325c7da9bdae7d750c0f8d
thinkgem <thinkgem@163.com>
2025-10-30 22:04:20 +0800

    add docker-compose.yml

---
+++ bin/docker-compose.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# /**
+#  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+#  * No deletion without permission, or be held responsible to law.
+#  *
+#  * Author: ThinkGem@163.com
+#  * 
+#  */
+echo ""
+echo "[信息] 部署Docker镜像。"
+echo ""
+
+echo "请按回车键继续 ... "
+read text
+
+cd ..
+
+# 停止并移除容器，如果存在
+docker compose -f docker-compose.yml stop
+docker compose -f docker-compose.yml rm -f
+
+# 运行容器
+docker compose -f docker-compose.yml up -d
+
+# 查看日志
+docker compose -f docker-compose.yml logs -f
+
+echo ""
+echo "启动完成 ... "
+echo ""
+
+cd bin
