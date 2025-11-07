289d30613ea2322f21ade8c669d6831bdbd25b81
wangy <2168607338@qq.com>
2025-11-07 13:10:27 +0800

    新增 docker-compose windows 脚本

---
+++ bin/docker-compose.bat
@@ -0,0 +1,34 @@
+@echo off
+rem /**
+rem  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+rem  * No deletion without permission, or be held responsible to law.
+rem  *
+rem  * Author: ThinkGem@163.com
+rem  */
+echo.
+echo [信息] 运行Docker镜像，使用 H2 内嵌数据库。
+echo.
+
+pause
+
+%~d0
+cd %~dp0
+
+cd ..
+
+rem 停止并移除容器，如果存在
+call docker-compose -f docker-compose.yml stop
+call docker-compose -f docker-compose.yml rm -f
+
+rem 运行容器
+call docker-compose -f docker-compose.yml up -d
+
+rem 查看日志
+call docker-compose -f docker-compose.yml logs -f
+
+echo .
+echo 启动完成 ... 
+echo .
+
+cd bin
+pause
