982a273fba1680c7fd4c9e455bf22a30bd9ad5e5
thinkgem <thinkgem@163.com>
2025-12-07 12:22:20 +0800

    重命名 jeesite-module-static 为 jeesite-module-core-static；移动 /modules/static 到 /modules/core/static

---
+++ modules/core/static/bin/deploy.bat
@@ -0,0 +1,25 @@
+@echo off
+rem /**
+rem  * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+rem  * No deletion without permission, or be held responsible to law.
+rem  *
+rem  * Author: ThinkGem@163.com
+rem  */
+echo.
+echo [信息] 部署工程到Maven服务器。
+echo.
+
+%~d0
+cd %~dp0
+
+if defined JAVA_HOME8 (
+  set "JAVA_HOME=%JAVA_HOME8%" & set "PATH=%JAVA_HOME8%\bin;%PATH%"
+)
+call mvn -v
+echo.
+
+cd ..
+call mvn clean deploy -Dmaven.test.skip=true -Pdeploy
+
+cd bin
+pause
