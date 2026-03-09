0a9ebf2f77b50931504a8ad93073579e9d9f47ab
thinkgem <thinkgem@163.com>
2026-03-09 15:40:50 +0800

    ObjectUtils增加转换整数时的兼容性

--- common/src/main/java/com/jeesite/common/lang/ObjectUtils.java
+++ common/src/main/java/com/jeesite/common/lang/ObjectUtils.java
@@ -56,7 +56,8 @@ public class ObjectUtils extends org.apache.commons.lang3.ObjectUtils {
 			return null;
 		}
 		try {
-			return new BigDecimal(val.toString().trim());
+			String str = val.toString();
+			return new BigDecimal(StringUtils.trim(str));
 		} catch (NumberFormatException e) {
 			return null;
 		}
@@ -86,7 +87,8 @@ public class ObjectUtils extends org.apache.commons.lang3.ObjectUtils {
 				}
 				return number == null ? 0D : number;
 			}
-			return Double.parseDouble(StringUtils.trim(str));
+			BigDecimal bd = new BigDecimal(StringUtils.trim(str));
+			return bd.doubleValue();
 		} catch (Exception e) {
 			return 0D;
 		}
@@ -123,7 +125,8 @@ public class ObjectUtils extends org.apache.commons.lang3.ObjectUtils {
 				}
 				return number == null ? 0L : number;
 			}
-			return Long.parseLong(StringUtils.trim(str));
+			BigDecimal bd = new BigDecimal(StringUtils.trim(str));
+			return bd.toBigInteger().longValue();
 		} catch (Exception e) {
 			return 0L;
 		}
