9023857cb4bbb38ea2a8b99f65ec06f31563b845
thinkgem <thinkgem@163.com>
2026-05-04 10:00:50 +0800

    更新 uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig, UserConfig, presetTypography, presetIcons, transformerDirectives } from 'unocss';
 import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders';
 import { presetWind3 } from '@unocss/preset-wind3';
+import { presetAntd } from '@antdv-next/unocss';
 
 export default defineConfig({
   inspector: false,
   content: {
     pipeline: {
-      include: ['**/*.vue', '**/*.tsx', '**/*.ts', '**/*-lib/**/*.mjs'],
+      include: ['**/*.vue', '**/*.tsx', '**/*.ts'],
       exclude: ['.git', '.idea', '.turbo', 'node_modules', 'public'],
     },
   },
   presets: [
+    presetAntd(),
     presetWind3(),
     presetTypography(),
     presetIcons({
